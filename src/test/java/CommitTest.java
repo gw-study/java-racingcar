7ba9706dbf846c5d90d96647c92cc6b4a775b825
lee.jaesung <lee.jaesung@joins.com>
2021-05-14 14:48:23 +0900

    test: 테스트 수정2

--- src/test/java/CommitTest.java
+++ src/test/java/CommitTest.java
@@ -5,6 +5,6 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class CommitTest {
     @Test
     void create_test() {
-        assertThat("String2").isEqualTo("String2");
+        assertThat("String3").isEqualTo("String3");
     }
 }
