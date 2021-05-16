dc508ec5e3c05f3e5ab008bea4a73df6fd967ad6
lee.jaesung <lee.jaesung@joins.com>
2021-05-17 07:27:57 +0900

    test: 리뷰 테스트 구현

---
+++ src/test/java/ReviewTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ReviewTest {
+    @Test
+    void create() {
+        assertThat("review1").isEqualTo("review1");
+    }
+}
