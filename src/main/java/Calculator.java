ad729765e7dc8b0a8469a6f6eb42d711e8a94d5a
kim.ahreum4 <kim.ahreum4@joins.com>
2021-06-11 12:23:51 +0900

    refactor : enum 삭제 map으로만 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,49 +1,30 @@
-import jdk.nashorn.internal.runtime.regexp.joni.constants.OPCode;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
 public class Calculator {
 
-    public enum OperationType{
-
-        ADDITION("+", (a, b) -> a + b),
-        SUBSTITUTION("-", (a, b) -> a - b),
-        MULTIPLY("*", (a, b) -> a * b),
-        DIVIDE("/", (a, b) -> a / b);
-
-        private String symbol;
-        private BiFunction<Integer, Integer, Integer> operation;
-
-        OperationType(String symbol, BiFunction<Integer, Integer, Integer> operation){
-            this.symbol = symbol;
-            this.operation = operation;
-        }
-
-        public int operate(int a, int b){
-            return operation.apply(a, b);
-        }
-
-    }
-
-    private static Map<String, OperationType> operators = new HashMap<>();
+    private static final Map<String, BiFunction<Integer, Integer, Integer>> operators = new HashMap<>();
 
     static {
-        for (OperationType value : OperationType.values())
-            operators.put(value.symbol, value);
+        operators.put("+", (a, b) -> a + b);
+        operators.put("-", (a, b) -> a - b);
+        operators.put("*", (a, b) -> a * b);
+        operators.put("/", (a, b) -> a / b);
     }
 
     public static int operate(String operator, int a, int b) {
-        if(!operators.containsKey(operator)){
+        if (!operators.containsKey(operator)) {
             throw new IllegalArgumentException("사칙 연산 기호가 아닙니다");
         }
-        return operators.get(operator).operate(a, b);
+
+        return operators.get(operator).apply(a, b);
     }
 
-    public int calculate(String input){
 
-        if(isBlank(input)){
+    public int calculate(String input) {
+
+        if (isBlank(input)) {
             throw new IllegalArgumentException("input data is blank or null");
         }
 
@@ -51,9 +32,10 @@ public class Calculator {
         int len = inputArr.length;
         int value = Integer.parseInt(inputArr[0]);
 
-        for(int i=1; i<len; i+=2){
-            value = operate(inputArr[i], value,Integer.parseInt(inputArr[i + 1]));
+        for (int i = 1; i < len; i += 2) {
+            value = operate(inputArr[i], value, Integer.parseInt(inputArr[i + 1]));
         }
+
         return value;
     }
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.platform.commons.util.StringUtils.isBlank;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("문자열 사칙 연산 계산기 구현")
 public class CalculatorTest {
@@ -15,32 +14,32 @@ public class CalculatorTest {
     @Test
     @DisplayName("덧셈 테스트")
     public void additionTest() {
-        assertThat(Calculator.OperationType.ADDITION.operate(10,5)).isEqualTo(15);
+        assertThat(Calculator.operate("+", 10, 5)).isEqualTo(15);
     }
 
     @Test
     @DisplayName("뺄셈 테스트")
     public void subtractionTest() {
-        assertThat(Calculator.OperationType.SUBSTITUTION.operate(10, 5)).isEqualTo(5);
+        assertThat(Calculator.operate("-", 10, 5)).isEqualTo(5);
     }
 
     @Test
     @DisplayName("곱셈 테스트")
     public void multiplicationTest() {
-        assertThat(Calculator.OperationType.MULTIPLY.operate(10, 5)).isEqualTo(50);
+        assertThat(Calculator.operate("*", 10, 5)).isEqualTo(50);
     }
 
     @Test
     @DisplayName("나눗셈 테스트")
-    public void divisionTest(){
-        assertThat(Calculator.OperationType.DIVIDE.operate(10,5)).isEqualTo(2);
+    public void divisionTest() {
+        assertThat(Calculator.operate("/", 10, 5)).isEqualTo(2);
     }
 
     @ParameterizedTest(name = "Input {0} is not supported.")
     @ValueSource(strings = {"", "  "})
     @DisplayName("입력값 NULL 또는 빈 공백 문자일 경우 예외 테스트")
     public void chkIsNullOrBlankTest(String input) {
-        assertThatThrownBy(() ->  calculator.calculate(input))
+        assertThatThrownBy(() -> calculator.calculate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageEndingWith("input data is blank or null");
     }
@@ -56,7 +55,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("사칙 연산을 모두 포함 하는 기능 테스트")
     public void calculatorTest() {
-        String input = "2 + 3 * 4 / 2";
-        assertEquals(10,calculator.calculate(input));
+        assertThat(calculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10);
     }
 }
