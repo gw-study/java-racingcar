24d76b4159ae457fae8f04da3f98ad7c79d7e4cf
park.kyoonho <toby369369@naver.com>
2021-06-11 14:40:51 +0900

    FEATURE: InpuView validation체크 로직 추가

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 class InputView {
     private static String QUESTION_ABOUT_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
     private static String QUESTION_ABOUT_NUMBER_OF_TIMES = "시도할 회수는 몇 회 인가요?";
-
+    private Scanner sc = new Scanner(System.in);
+    
     public int inputNumberOfCar() {
         System.out.println(QUESTION_ABOUT_NUMBER_OF_CARS);
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        return isValidInput(sc.nextLine());
     }
 
     public int inputNumberOfTimes() {
         System.out.println(QUESTION_ABOUT_NUMBER_OF_TIMES);
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        return isValidInput(sc.nextLine());
+    }
+
+    public int isValidInput(String input) throws NullPointerException {
+        String regex = "^[0-9]+$";
+
+        if (!input.matches(regex)) {
+            throw new NullPointerException();
+        }
+
+        return Integer.parseInt(input);
     }
 }
