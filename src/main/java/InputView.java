4b51615a5803ebdf26ac285091a0ef8b2f5abf1b
a-ri-mu <arumu215@gmail.com>
2021-06-16 09:24:04 +0900

    feature : inputView, resultView 기능 수정

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class InputView {
     Scanner scanner = new Scanner(System.in);
 
-    int carCount() {
+    public int carCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return scanner.nextInt();
     }
 
-    int tryCount() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
+    public int tryCount() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
         return scanner.nextInt();
     }
 }
--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class ResultView {
     void printCar(String[] carArray) {
+        System.out.println();
         Arrays.stream(carArray).forEach(System.out::println);
     }
 }
