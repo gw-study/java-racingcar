ef20dc37237e9d12926ffe5d94ba10dcec6e585a
dahyunja <dlekgus0220@gmail.com>
2021-06-23 23:21:41 +0900

    refactor : 자동차의 상태를 출력하는 기능 수정

--- src/main/java/racingcar/step3/ResultView.java
+++ src/main/java/racingcar/step3/ResultView.java
@@ -1,19 +1,26 @@
 package racingcar.step3;
 
+import java.util.ArrayList;
+
 public class ResultView {
-    public static void resultViewStart() {
+
+    public static void resultView(RacingCar racingCar, ArrayList movementOfCar) {
+        System.out.println();
         System.out.println("실행 결과");
+        for (int i=0; i < racingCar.getCountOfRounds(); i++){
+            printNumberOfCars(racingCar, movementOfCar);
+        }
     }
 
-    public static void resultView(int[] cars, int seq) {
-        for (int i=0; i < cars.length; i++){
-            print(cars[i]);
+    public static void printNumberOfCars(RacingCar racingCar, ArrayList movementOfCar) {
+        for (int i=0; i < racingCar.getCountOfCars(); i++){
+            printNumberOfMovement((Integer) movementOfCar.remove(0));
         }
         System.out.println();
     }
 
-    public static void print(int cars){
-        for (int i=0; i < cars; i++){
+    public static void printNumberOfMovement(int numberOfMovement){
+        for (int i=0; i < numberOfMovement; i++){
             System.out.print("-");
         }
         System.out.println();
