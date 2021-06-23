e37b215499de3dc1d16633c79c3e789c97230605
dahyunja <dlekgus0220@gmail.com>
2021-06-23 23:24:09 +0900

    test : 기능 테스트 코드 수정

--- src/test/java/racingcar/step3/RacingCarTest.java
+++ src/test/java/racingcar/step3/RacingCarTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 class RacingCarTest {
     RacingCar racingCar;
 
@@ -15,37 +17,32 @@ class RacingCarTest {
     @Test
     @DisplayName("자동차 수 입력값 테스트")
     void getRacingCarInput() {
-        racingCar.getRacingCarInput();
-    }
-
-    @Test
-    @DisplayName("시도 횟수 입력값 테스트")
-    void getRacingCars() {
-        racingCar.getRacingCars();
+        assertThat(racingCar.getCountOfCars()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("자동차 수 배열 생성 테스트")
+    void getRacingCars() {
+        assertThat(racingCar.getCars()).isEqualTo(new int[3]);
+    }
+
+    @Test
+    @DisplayName("시도 횟수 입력값 테스트")
     void getRacingNumberInput() {
-        racingCar.getRacingNumberInput();
+        assertThat(racingCar.getCountOfRounds()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("랜덤값 생성 테스트")
     void makeRandomNumber() {
-        RandomNumber.makeRandomNumber();
-    }
-
-    @Test
-    @DisplayName("자동차 경주 테스트")
-    void racing() {
-        RacingCarMain.racing(racingCar);
+        assertThat(RandomNumber.makeRandomNumber()).isBetween(0,9);
     }
 
     @Test
     @DisplayName("자동차 경주 1회 전진 테스트")
-    void racingOnce() {
-        RacingCarMain.racingOnce(racingCar, racingCar.getRacingCars());
+    void move() {
+        RacingCarMain.move(racingCar, racingCar.getCars(), racingCar.getMovementOfCar());
+        assertThat(racingCar.getMovementOfCar().size()).isEqualTo(racingCar.getCountOfCars());
     }
 
 }
