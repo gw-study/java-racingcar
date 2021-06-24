76dfd299eae1a9e8398937656cc228bd64c4e758
a-ri-mu <arumu215@gmail.com>
2021-06-24 22:16:52 +0900

    REFACTOR  : Car.java 현재위치와 움직이는 행동만 정의

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,39 +1,16 @@
-import java.util.Random;
-
 public class Car {
+
     private static final int RANDOM_LIMIT_NUM = 4;
+    private String curPosition = "";
 
-    public int makeRandom() {
-        Random random = new Random();
-        return random.nextInt(10);
-    }
-
-    public String addMove(String str) {
-        StringBuilder stringBuilder = new StringBuilder(str);
-        stringBuilder.append("-");
-        return stringBuilder.toString();
-    }
-
-    public String[] initArray(String[] stringArr) {
-        for (int i = 0; i < stringArr.length; i++) {
-            stringArr[i] = "";
+    public String move(int value) {
+        if (value >= RANDOM_LIMIT_NUM) {
+            curPosition += "-";
         }
-        return stringArr;
+        return curPosition;
     }
 
-    public void racingStart(int carCnt, int tryCnt) {
-        String[] carArray = new String[carCnt];
-        ResultView resultView = new ResultView();
-
-        carArray = initArray(carArray);
-
-        for (int i = 0; i < tryCnt; i++) {
-            for (int j = 0; j < carArray.length; j++) {
-                if (makeRandom() >= RANDOM_LIMIT_NUM) {
-                    carArray[j] = addMove(carArray[j]);
-                }
-            }
-            resultView.printCar(carArray);
-        }
+    public String getCurPosition() {
+        return curPosition;
     }
 }
