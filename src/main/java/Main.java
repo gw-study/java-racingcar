68b9ece0055ba1018030847cf8d8d59361e184fb
a-ri-mu <arumu215@gmail.com>
2021-06-24 22:21:51 +0900

    REFACTOR : Main.java 컨트롤러 역할로 추가, ResultView.java 수정

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+
+        RacingCar racingCar = new RacingCar();
+        racingCar.initCars(inputView.carCount());
+        racingCar.initTryCnt(inputView.tryCount());
+
+        ResultView resultView = new ResultView(racingCar);
+        resultView.printCar();
+    }
+}
--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -1,8 +1,18 @@
-import java.util.Arrays;
-
 public class ResultView {
-    void printCar(String[] carArray) {
+    private final RacingCar racingCar;
+
+    public ResultView(RacingCar racingCar) {
+        this.racingCar = racingCar;
+    }
+
+    public void printCar() {
         System.out.println();
-        Arrays.stream(carArray).forEach(System.out::println);
+        System.out.println("실행 결과");
+        for (int i = 0; i < racingCar.tryCnt; i++) {
+            System.out.println();
+            for (Car car : racingCar.start()) {
+                System.out.println(car.getCurPosition());
+            }
+        }
     }
 }
