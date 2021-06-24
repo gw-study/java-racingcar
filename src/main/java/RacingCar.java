e5a3ad565920d7390b50f9dc14a5889ac9aeaea0
a-ri-mu <arumu215@gmail.com>
2021-06-24 22:19:21 +0900

    REFACTOR : RacingCar.java 레이싱 관련 정의, 메소드별 책임 분리, 메소드명 정리 등

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,8 +1,30 @@
-public class RacingCar {
-    public static void main(String[] args) {
-        InputView inputView = new InputView();
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-        Car car = new Car();
-        car.racingStart(inputView.carCount(), inputView.tryCount());
+public class RacingCar {
+
+    private final List<Car> carList = new ArrayList<>();
+    public int tryCnt;
+
+    public void initCars(int carCnt) {
+        for (int i = 0; i < carCnt; i++) {
+            carList.add(new Car());
+        }
+    }
+
+    public void initTryCnt(int tryCnt) {
+        this.tryCnt = tryCnt;
+    }
+
+    private int makeRandom() {
+        return new Random().nextInt(10);
+    }
+
+    public List<Car> start() {
+        for (Car car : carList) {
+            car.move(makeRandom());
+        }
+        return carList;
     }
 }
