8bdf8ef84723926d6ca860c246c960fc73aeb43e
park.kyoonho <toby369369@naver.com>
2021-06-28 10:22:43 +0900

    REFACTOR: InputView, ResultView 수정

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class InputView {
+    private final static String QUESTION_ABOUT_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private final static String QUESTION_ABOUT_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
     private final static String QUESTION_ABOUT_NUMBER_OF_TIMES = "시도할 회수는 몇 회 인가요?";
     private Scanner sc = new Scanner(System.in);
 
-    public int inputNumberOfCar() {
-        System.out.println(QUESTION_ABOUT_NUMBER_OF_CARS);
-        return isValidInput(sc.nextLine());
+    public String[] inputNameOfCars() {
+        System.out.println(QUESTION_ABOUT_NAME_OF_CARS);
+        return isValidString(sc.nextLine());
     }
 
     public int inputNumberOfTimes() {
@@ -23,5 +24,33 @@ public class InputView {
         }
 
         return Integer.parseInt(input);
+    }
+
+    public String[] canSplit(String input) throws IllegalArgumentException {
+
+        if (input.contains(",")) {
+            String[] inputs = input.split(",");
+
+            for (String name: inputs) {
+                if (isEmpty(name)) {
+                    throw new IllegalArgumentException();
+                }
+            }
+
+            return inputs;
+        }
+
+        String[] name = new String[1];
+        name[0] = input;
+
+        return name;
+    }
+
+    public boolean isEmpty(String name) {
+        return name.isEmpty();
+    }
+
+    public String[] isValidString(String input) {
+        return canSplit(input);
     }
 }
--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -1,21 +1,43 @@
-class ResultView {
-    private static String textOfResult = "결과보기";
-    private RacingCarCompetition competition;
+import java.util.List;
 
-    ResultView(RacingCarCompetition competition) {
+public class ResultView {
+    private final static String textOfResult = "결과보기";
+    private final static String suffixOfText = "가 최종 우승했습니다.";
+    private List<Car[]> histories;
+    private List<Car> winners;
+
+    public ResultView(List<Car[]> histories, List<Car> winners) {
         System.out.println();
         System.out.println(textOfResult);
-        this.competition = competition;
+        this.histories = histories;
+        this.winners = winners;
     }
 
     public void showDistance() {
-        int historySize = competition.history.size();
+        int historySize = histories.size();
 
-        for (int j = 0; j < historySize; j++) {
-            for (String distance : competition.history.get(j)) {
-                System.out.println(distance);
+        for (int i = 0; i < historySize; i++) {
+            for (Car car : histories.get(i)) {
+                System.out.println(car.name + ": " + car.position.distance);
             }
             System.out.println();
         }
+    }
+
+    public void showWinner() {
+        int numberOfWinner = winners.size();
+        String alertOfWinner = "";
+
+        for (int i = 0; i < numberOfWinner; i++) {
+            alertOfWinner += winners.get(i).name;
+
+            if (numberOfWinner != 1 && i != numberOfWinner - 1) {
+                alertOfWinner += ", ";
+            }
+        }
+
+        alertOfWinner += suffixOfText;
+
+        System.out.println(alertOfWinner);
     }
 }
