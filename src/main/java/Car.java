ece0f5294608729b2cca4799dac52d7ed5941aa3
park.kyoonho <toby369369@naver.com>
2021-06-28 10:21:13 +0900

    REFACTOR: sort를위한 comparable 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,9 +1,11 @@
-public class Car {
+public class Car implements Comparable<Car> {
     private final static int LIMIT_RANDOM_NUMBER = 4;
     public Position position;
+    public String name;
 
-    Car(String defaultDistance, int defaultValue) {
-        position = new Position(defaultDistance, defaultValue);
+    public Car(String defaultDistance, int defaultValue, String name) {
+        this.name = name;
+        this.position = new Position(defaultDistance, defaultValue);
     }
 
     public void move(int randomNumber) {
@@ -16,5 +18,16 @@ public class Car {
 
     public int getRandomNumber() {
         return (int)(Math.random() * 10);
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        if (this.position.value < car.position.value) {
+            return -1;
+        } else if (this.position.value == car.position.value) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
