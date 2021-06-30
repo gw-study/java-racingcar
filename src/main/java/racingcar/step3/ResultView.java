3da31bb89dbbeaa9bffa1dc64d15425004bd03ca
dahyunja <dlekgus0220@gmail.com>
2021-07-01 01:28:18 +0900

    refactor : 자동차 경주 게임의 우승자를 출력하는 기능

--- src/main/java/racingcar/step3/ResultView.java
+++ src/main/java/racingcar/step3/ResultView.java
@@ -4,22 +4,27 @@ import java.util.ArrayList;
 
 public class ResultView {
 
-    public static void resultView(RacingCar racingCar, ArrayList movementOfCar) {
+    public void resultView(RacingCar racingCar) {
         System.out.println();
         System.out.println("실행 결과");
         for (int i=0; i < racingCar.getCountOfRounds(); i++){
-            printNumberOfCars(racingCar, movementOfCar);
+            printNumberOfCars(racingCar);
         }
+        for (int i=0; i < racingCar.getWinner().size()-1; i++){
+            System.out.print(racingCar.getWinner().get(i)+", ");
+        }
+        System.out.println(racingCar.getWinner().get(racingCar.getWinner().size()-1)+"가 최종 우승했습니다.");
     }
 
-    public static void printNumberOfCars(RacingCar racingCar, ArrayList movementOfCar) {
+    public void printNumberOfCars(RacingCar racingCar) {
         for (int i=0; i < racingCar.getCountOfCars(); i++){
-            printNumberOfMovement((Integer) movementOfCar.remove(0));
+            System.out.print(racingCar.getInfoOfCars().get(i).getNameOfCar()+" : ");
+            printNumberOfMovement((Integer) racingCar.getMovementOfCar().remove(0));
         }
         System.out.println();
     }
 
-    public static void printNumberOfMovement(int numberOfMovement){
+    public void printNumberOfMovement(int numberOfMovement){
         for (int i=0; i < numberOfMovement; i++){
             System.out.print("-");
         }
