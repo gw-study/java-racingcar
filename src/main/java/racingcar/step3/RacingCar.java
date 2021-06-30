5798d0564062b879f02a7e5c83d378455aac0e09
dahyunja <dlekgus0220@gmail.com>
2021-07-01 01:27:43 +0900

    refactor : 자동차 경주 게임의 우승자를 구하는 기능

--- src/main/java/racingcar/step3/RacingCar.java
+++ src/main/java/racingcar/step3/RacingCar.java
@@ -1,18 +1,27 @@
 package racingcar.step3;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class RacingCar {
+    private final String nameOfCars;
+    private ArrayList<Car> infoOfCars;
     private final int countOfCars;
     private final int countOfRounds;
-    private int[] cars;
-    private ArrayList movementOfCar;
+    private ArrayList<Integer> movementOfCar;
+    private ArrayList<String> winner;
+    private final int MOVE_CONDITION = 4;
 
-    public RacingCar(int countOfCars, int countOfRounds) {
-        this.countOfCars = countOfCars;
+    public RacingCar(String nameOfCars, int countOfRounds) {
+        this.nameOfCars = nameOfCars;
+        this.infoOfCars = enterNameOfCars(nameOfCars);
+        this.countOfCars = infoOfCars.size();
         this.countOfRounds = countOfRounds;
-        this.cars = new int[countOfCars];
         this.movementOfCar = new ArrayList<>();
+        this.winner = new ArrayList<>();
+    }
+
+    public String getNameOfCars(){
+        return nameOfCars;
     }
 
     public int getCountOfCars(){
@@ -23,11 +32,57 @@ public class RacingCar {
         return countOfRounds;
     }
 
-    public int[] getCars(){
-        return cars;
+    public ArrayList<Car> getInfoOfCars(){
+        return infoOfCars;
     }
 
-    public ArrayList getMovementOfCar(){
+    public ArrayList<Integer> getMovementOfCar(){
         return movementOfCar;
+    }
+
+    public ArrayList<String> getWinner(){
+        return winner;
+    }
+
+    public void race(RacingCar racingCar) {
+        for (int i=0; i < countOfCars; i++){
+            racingCar.move(i);
+            movementOfCar.add(getInfoOfCars().get(i).getPositionOfCar());
+        }
+    }
+
+    public void move(int i) {
+        if (RandomNumber.makeRandomNumber() >= MOVE_CONDITION) {
+            getInfoOfCars().get(i).moveCar();
+        }
+    }
+
+    public ArrayList<Car> enterNameOfCars(String nameOfCars){
+        String[] namesOfCar = nameOfCars.split(",");
+        infoOfCars = new ArrayList<>();
+        for (int i=0; i < namesOfCar.length; i++){
+            infoOfCars.add(new Car(namesOfCar[i], 0));
+        }
+        return infoOfCars;
+    }
+
+    public ArrayList<String> findWinner(ArrayList<Car> infoOfCars){
+        winner = new ArrayList<>();
+        int[] position = new int[countOfCars];
+        for (int i=0; i < countOfCars; i++){
+            position[i] = infoOfCars.get(i).getPositionOfCar();
+        }
+        Arrays.sort(position);
+        int max = position[countOfCars-1];
+        for (int i=0; i < countOfCars; i++) {
+            addWinner(max, i);
+        }
+        return winner;
+    }
+
+    public void addWinner(int max, int i){
+        if(infoOfCars.get(i).getPositionOfCar() == max){
+            winner.add(infoOfCars.get(i).getNameOfCar());
+        }
     }
 }
--- src/main/java/racingcar/step3/RacingCarMain.java
+++ src/main/java/racingcar/step3/RacingCarMain.java
@@ -1,26 +1,15 @@
 package racingcar.step3;
 
-import java.util.ArrayList;
-
 public class RacingCarMain {
-    private static final int MOVE_CONDITION = 4;
 
     public static void main(String[] args) {
-        RacingCar racingCar = InputView.inputView();
-        int[] cars = racingCar.getCars();
-        ArrayList movementOfCar = racingCar.getMovementOfCar();
+        InputView inputView = new InputView();
+        RacingCar racingCar = inputView.inputView();
         for (int i=0; i < racingCar.getCountOfRounds(); i++){
-            move(racingCar, cars, movementOfCar);
+            racingCar.race(racingCar);
         }
-        ResultView.resultView(racingCar, movementOfCar);
-    }
-
-    public static void move(RacingCar racingCar, int[] cars, ArrayList movementOfCar) {
-        for (int i=0; i < racingCar.getCountOfCars(); i++){
-            if (RandomNumber.makeRandomNumber() >= MOVE_CONDITION) {
-                cars[i]++;
-            }
-            movementOfCar.add(cars[i]);
-        }
+        racingCar.findWinner(racingCar.getInfoOfCars());
+        ResultView resultView = new ResultView();
+        resultView.resultView(racingCar);
     }
 }
