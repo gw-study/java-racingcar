a9f14c0fa7caa5c33d77d730851ebeeea57c9dd0
park.kyoonho <toby369369@naver.com>
2021-07-03 22:37:02 +0900

    refactor: 매직넘버 제거, 스트림방식을 이용한 max값 구하기

--- src/main/java/RacingCarCompetition.java
+++ src/main/java/RacingCarCompetition.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class RacingCarCompetition {
 
     private final int numberOfCar;
     private final int numberOfTimes;
     private final String[] names;
+    private final Round round;
 
     public List<Car> cars = new ArrayList<>();
     public List<Car[]> history = new ArrayList<>();
@@ -16,41 +14,50 @@ public class RacingCarCompetition {
         this.names = names;
         this.numberOfTimes = numberOfTimes;
         this.numberOfCar = names.length;
+        this.round = new Round(numberOfTimes);
         readyForCar();
     }
 
-    public void readyForCar() {
+    private void readyForCar() {
         for (int i = 0; i < numberOfCar; i++) {
             cars.add(new Car("-", 1, names[i]));
         }
     }
 
     public List<Car[]> startRacing() {
-        for (int i = 0; i < numberOfTimes; i++) {
-            Car[] c = new Car[numberOfCar];
 
-            for (int j = 0 ; j < numberOfCar ; j++) {
-                cars.get(j).move(cars.get(j).getRandomNumber());
-                c[j] = new Car(cars.get(j).position.distance, cars.get(j).position.value, cars.get(j).name);
-            }
-
-            history.add(i, c);
+        while (round.isContinuedRound()) {
+            start();
+            round.reduceRound();
         }
 
         return history;
     }
 
+    public void start() {
+        Car[] c = new Car[numberOfCar];
+
+        for (int j = 0 ; j < numberOfCar ; j++) {
+            cars.get(j).move(cars.get(j).getRandomNumber());
+            c[j] = new Car(cars.get(j).getPositionString(), cars.get(j).getPositionInteger(), cars.get(j).getName());
+        }
+
+        history.add(c);
+    }
+
     public List<Car> getWinner() {
-        Car[] last = history.get(numberOfTimes-1);
-        Arrays.sort(last, Collections.reverseOrder());
+        int lastIndex = numberOfTimes-1;
+        Car[] last = history.get(lastIndex);
 
         List<Car> winners = new ArrayList<>();
-        Car first = new Car(last[0].position.distance, last[0].position.value, last[0].name);
+
+        Car winner = Arrays.stream(last).max(Comparator.comparingInt(Car::getPositionInteger)).orElseThrow(NoSuchElementException::new);
+        Car first = new Car(last[0].getPositionString(), last[0].getPositionInteger(), last[0].getName());
 
         winners.add(0, first);
 
         for (int i = 1; i < last.length; i++) {
-            if (first.position.value == last[i].position.value) {
+            if (winner.getPositionInteger() == last[i].getPositionInteger()) {
                 winners.add(last[i]);
             }
         }
