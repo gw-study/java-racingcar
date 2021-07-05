e8d39ea08283cb7c31e70a0186c887da2377a993
dahyunja <dlekgus0220@gmail.com>
2021-07-06 00:51:56 +0900

    refactor : 자동차 경주 게임의 우승자를 출력하는 기능

--- src/main/java/racingcar/step3/ResultView.java
+++ src/main/java/racingcar/step3/ResultView.java
@@ -1,31 +1,29 @@
 package racingcar.step3;
 
-import java.util.ArrayList;
-
 public class ResultView {
 
-    public void resultView(RacingCar racingCar) {
+    public void resultView(RacingCar racingCar, Winner winner) {
         System.out.println();
         System.out.println("실행 결과");
-        for (int i=0; i < racingCar.getCountOfRounds(); i++){
+        for (int i = 0; i < racingCar.getCountOfRounds(); i++) {
             printNumberOfCars(racingCar);
         }
-        for (int i=0; i < racingCar.getWinner().size()-1; i++){
-            System.out.print(racingCar.getWinner().get(i)+", ");
+        for (int i = 0; i < winner.getWinner().size() - 1; i++) {
+            System.out.print(winner.getWinner().get(i) + ", ");
         }
-        System.out.println(racingCar.getWinner().get(racingCar.getWinner().size()-1)+"가 최종 우승했습니다.");
+        System.out.println(winner.getWinner().get(winner.getWinner().size() - 1) + "가 최종 우승했습니다.");
     }
 
-    public void printNumberOfCars(RacingCar racingCar) {
-        for (int i=0; i < racingCar.getCountOfCars(); i++){
-            System.out.print(racingCar.getInfoOfCars().get(i).getNameOfCar()+" : ");
+    private void printNumberOfCars(RacingCar racingCar) {
+        for (int i = 0; i < racingCar.getCountOfCars(); i++) {
+            System.out.print(racingCar.getInfoOfCars().get(i).getNameOfCar() + " : ");
             printNumberOfMovement((Integer) racingCar.getMovementOfCar().remove(0));
         }
         System.out.println();
     }
 
-    public void printNumberOfMovement(int numberOfMovement){
-        for (int i=0; i < numberOfMovement; i++){
+    private void printNumberOfMovement(int numberOfMovement) {
+        for (int i = 0; i < numberOfMovement; i++) {
             System.out.print("-");
         }
         System.out.println();
