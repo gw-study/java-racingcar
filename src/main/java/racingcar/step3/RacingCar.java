85b95cc333350789293f0145db85cc18f9a8ead3
dahyunja <dlekgus0220@gmail.com>
2021-07-06 00:51:01 +0900

    refactor : 자동차가 주어진 횟수 동안 랜덤으로 전진 또는 멈추는 기능

--- src/main/java/racingcar/step3/RacingCar.java
+++ src/main/java/racingcar/step3/RacingCar.java
@@ -1,88 +1,54 @@
 package racingcar.step3;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class RacingCar {
-    private final String nameOfCars;
-    private ArrayList<Car> infoOfCars;
-    private final int countOfCars;
+    private static final int MOVE_CONDITION = 4;
     private final int countOfRounds;
+    private ArrayList<Car> infoOfCars;
     private ArrayList<Integer> movementOfCar;
-    private ArrayList<String> winner;
-    private final int MOVE_CONDITION = 4;
 
     public RacingCar(String nameOfCars, int countOfRounds) {
-        this.nameOfCars = nameOfCars;
         this.infoOfCars = enterNameOfCars(nameOfCars);
-        this.countOfCars = infoOfCars.size();
         this.countOfRounds = countOfRounds;
         this.movementOfCar = new ArrayList<>();
-        this.winner = new ArrayList<>();
     }
 
-    public String getNameOfCars(){
-        return nameOfCars;
+    public ArrayList<Car> getInfoOfCars() {
+        return infoOfCars;
     }
 
-    public int getCountOfCars(){
-        return countOfCars;
-    }
-
-    public int getCountOfRounds(){
+    public int getCountOfRounds() {
         return countOfRounds;
     }
 
-    public ArrayList<Car> getInfoOfCars(){
-        return infoOfCars;
+    public int getCountOfCars() {
+        return infoOfCars.size();
     }
 
-    public ArrayList<Integer> getMovementOfCar(){
+    public ArrayList<Integer> getMovementOfCar() {
         return movementOfCar;
     }
 
-    public ArrayList<String> getWinner(){
-        return winner;
-    }
-
-    public void race(RacingCar racingCar) {
-        for (int i=0; i < countOfCars; i++){
-            racingCar.move(i);
-            movementOfCar.add(getInfoOfCars().get(i).getPositionOfCar());
-        }
-    }
-
-    public void move(int i) {
-        if (RandomNumber.makeRandomNumber() >= MOVE_CONDITION) {
-            getInfoOfCars().get(i).moveCar();
-        }
-    }
-
-    public ArrayList<Car> enterNameOfCars(String nameOfCars){
+    private ArrayList<Car> enterNameOfCars(String nameOfCars) {
         String[] namesOfCar = nameOfCars.split(",");
         infoOfCars = new ArrayList<>();
-        for (int i=0; i < namesOfCar.length; i++){
-            infoOfCars.add(new Car(namesOfCar[i], 0));
+        for (String carName : namesOfCar) {
+            infoOfCars.add(new Car(carName, 0));
         }
         return infoOfCars;
     }
 
-    public ArrayList<String> findWinner(ArrayList<Car> infoOfCars){
-        winner = new ArrayList<>();
-        int[] position = new int[countOfCars];
-        for (int i=0; i < countOfCars; i++){
-            position[i] = infoOfCars.get(i).getPositionOfCar();
+    public void race(RacingCar racingCar) {
+        for (int i = 0; i < infoOfCars.size(); i++) {
+            racingCar.move(i, RandomNumber.makeRandomNumber());
+            infoOfCars.get(i).addPositionOfCar(movementOfCar);
         }
-        Arrays.sort(position);
-        int max = position[countOfCars-1];
-        for (int i=0; i < countOfCars; i++) {
-            addWinner(max, i);
-        }
-        return winner;
     }
 
-    public void addWinner(int max, int i){
-        if(infoOfCars.get(i).getPositionOfCar() == max){
-            winner.add(infoOfCars.get(i).getNameOfCar());
+    public void move(int i, int randomNumber) {
+        if (randomNumber >= MOVE_CONDITION) {
+            infoOfCars.get(i).moveCar();
         }
     }
 }
