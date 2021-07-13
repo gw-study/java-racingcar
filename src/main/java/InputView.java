f617bf0de5375be218bd93a22144aaa400eee9e5
park.kyoonho <toby369369@naver.com>
2021-07-13 11:16:48 +0900

    refactor: 미사용 맴버변수 제거

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class InputView {
     private final static String QUESTION_ABOUT_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private final static String QUESTION_ABOUT_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
     private final static String QUESTION_ABOUT_NUMBER_OF_TIMES = "시도할 회수는 몇 회 인가요?";
+
     private Scanner sc = new Scanner(System.in);
 
     public String[] inputNameOfCars() {
